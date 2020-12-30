7706bf92d00c4e0ee9cacffba0105ef7fb8a55b2
djanuskaite <67839604+djanuskaite@users.noreply.github.com>
2020-12-30 20:21:37 +0200

    Add files via upload

---
+++ Projects/UseEffect_and_ConditionalRendering/Reviews/Review.js
@@ -0,0 +1,66 @@
+import React, { useState } from 'react';
+import people from './data';
+import { FaChevronLeft, FaChevronRight, FaQuoteRight } from 'react-icons/fa';
+
+const Review = () => {
+  const [index, setIndex] = useState(0);
+  const { name, job, image, text } = people[index];
+
+  const checkNumber = (number) => {
+    if (number > people.length - 1) {
+      return 0;
+    }
+    if (number < 0) {
+      return people.length - 1;
+    }
+    return number;
+  };
+
+  const nextPerson = () => {
+    setIndex((index) => {
+      let newIndex = index + 1 // +1 bcs I want to see a next person
+      return checkNumber(newIndex);
+    });
+  };
+  const prevPerson = () => {
+    setIndex((index) => {
+      let newIndex = index - 1 // +1 bcs I want to see a prev person
+      return checkNumber(newIndex);
+    });
+  };
+
+  const randomPerson = () => {
+    // just zmoniu id numberiai between 0-3
+    let randomNumber = Math.floor(Math.random() * people.length);
+    // to avoid repetition, shows random person
+    if (randomNumber === index) {
+      randomNumber = index + 1;
+    }
+    setIndex(checkNumber(randomNumber));
+  };
+
+  return (
+    <article className='review'>
+      <div className='img-container'>
+        <img src={image} alt={name} className='person-img' />
+        <span className='quote-icon'>
+          <FaQuoteRight />
+        </span>
+      </div>
+      <h4 className='author'>{name}</h4>
+      <p className='job'>{job}</p>
+      <p className='info'>{text}</p>
+      <div className='button-container'>
+        <button className='prev-btn' onClick={prevPerson}>
+          <FaChevronLeft />
+        </button>
+        <button className='next-btn' onClick={nextPerson}>
+          <FaChevronRight />
+        </button>
+      </div>
+      <button className='random-btn' onClick={randomPerson}>Surprise Me</button>
+    </article>
+  );
+};
+
+export default Review;
