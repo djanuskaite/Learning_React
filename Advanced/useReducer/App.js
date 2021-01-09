661666aa9384484bc582c6180fe0458c6ac275dd
djanuskaite <67839604+djanuskaite@users.noreply.github.com>
2021-01-09 22:31:39 +0200

    Add files via upload

---
+++ Advanced/useReducer/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Final from './tutorial/6-useReducer/final'; // kai cia pakeiciam setup folderio pavad, gaunam kita pvz
+
+function App() {
+  return (
+    <div className='container'>
+      <Final />
+    </div>
+  )
+}
+
+export default App
