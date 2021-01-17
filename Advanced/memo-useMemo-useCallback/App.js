277c3aa831e78c6318c6f88020ad5d8fcc05305a
djanuskaite <67839604+djanuskaite@users.noreply.github.com>
2021-01-17 20:51:20 +0200

    Add files via upload

---
+++ Advanced/memo-useMemo-useCallback/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Setup from './tutorial/12-memo-useMemo-useCallback/setup'; // kai cia pakeiciam setup folderio pavad, gaunam kita pvz
+
+//everything's happening on a client side, we are not refreshing the page
+
+function App() {
+  return (
+    <div className='container'>
+      <Setup />
+    </div>
+  )
+}
+
+export default App
