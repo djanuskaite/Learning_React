a2cc156a1183dde40fc41049dabff0c4c59d35c2
djanuskaite <67839604+djanuskaite@users.noreply.github.com>
2021-01-17 13:00:22 +0200

    Add files via upload

---
+++ Advanced/Prop Drilling/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Setup from './tutorial/7-prop-drilling/setup/1-prop-drilling'; // kai cia pakeiciam setup folderio pavad, gaunam kita pvz
+
+function App() {
+  return (
+    <div className='container'>
+      <Setup />
+    </div>
+  )
+}
+
+export default App
